c84d5ea8ef07fb81d6ea03d9cf3426afc0a6d90b
niwa-niwa <urban.hang274@gmail.com>
2023-06-11 14:22:20 +0900

    implemented ServeStatic for rendering html

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
+import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersService } from "./users/users.service";
+import { UsersController } from "./users/users.controller";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/user.entity";
+import { join } from "path";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: "mysql",
+      host: "localhost",
       port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'nestdb',
+      username: "root",
+      password: "password",
+      database: "nestdb",
       entities: [User],
       synchronize: true,
     }),
     UsersModule,
-    ],
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "frontend", "build"),
+      exclude: ["/api/(.*)"],
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
